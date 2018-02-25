c1a1287d6aeb39f440762c5d0ee13136abd82d1a
FcGod <36422731+FcGod@users.noreply.github.com>
2018-02-26 00:52:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -495,6 +495,12 @@ client.on("message", async message => {
           member.setNickname(name)
   }
 
+  if(command === "ram"){
+    const arr = [1, 2, 3, 4, 5, 6, 9, 7, 8, 9, 10];
+    arr.reverse();
+    const used = process.memoryUsage().heapUsed / 1024 / 1024;
+    message.channel.send(`Approx. RAM usage. ${Math.round(used * 100) / 100} MB`);
+  }
 
   
 
